a20c639697ea34954f09d28b4ba5e30a492cfcfa
Oliseh <studyug@hotmail.com>
2025-08-14 16:38:43 +0300

    fixed app connection

--- config/index.tsx
+++ config/index.tsx
@@ -1,6 +1,7 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { celo, base } from '@reown/appkit/networks'
 import { farcasterFrame } from "@farcaster/miniapp-wagmi-connector"
+import { createAppKit } from '@reown/appkit/react'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 import type { Config } from 'wagmi'
 import type { AppKitNetwork } from '@reown/appkit/networks'
@@ -34,6 +35,40 @@ export const wagmiAdapter = new WagmiAdapter({
   networks,
   projectId,
   ssr: true
+})
+
+// Create AppKit instance with all features enabled
+// The conditional logic will be handled in the UI components
+export const appKit = createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  defaultNetwork: base,
+  metadata: {
+    name: 'Snarkels',
+    description: 'Snarkels - Create and host interactive quizzes',
+    url: 'https://snarkels.lol',
+    icons: ['https://snarkels.lol/logo.png']
+  },
+  features: {
+    analytics: true,
+    // Enable all authentication features by default
+    email: true,
+    socials: [
+      "google",
+      "x", 
+      "github",
+      "discord",
+      "apple",
+      "facebook",
+      "farcaster",
+    ],
+    emailShowWallets: true,
+  },
+  // Show all wallets
+  allWallets: "SHOW",
+  // Set theme to white
+  themeMode: "light"
 })
 
 // Export the wagmi config for direct use
